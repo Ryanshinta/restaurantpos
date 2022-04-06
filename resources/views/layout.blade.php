acdb6d190a44ba4e08a6c60498fb400afb0e7192
AshChan016 <69001262+AshChan016@users.noreply.github.com>
2022-04-06 10:31:06 +0800

    Add files via upload

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -31,9 +31,9 @@
                         <a href="#" class="sub-item">Cancel Order</a>
                     </div>
                 </div>
-                <div class="item"><a href="/payment">Payment</a></div>
+                <div class="item"><a href="#">Payment</a></div>
                 <div class="item"><a href="#">Voucher</a></div>
-                <div class="item"><a href="/restaurantTable">Table</a></div>
+                <div class="item"><a href="/restauranttable">Table</a></div>
                 <div class="item"><a href="/reservation">Reservation</a></div>
                 <div class="item"><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a></div>
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
@@ -45,7 +45,7 @@
         <div>
             @yield('content')
         </div>
-    </body>
+    
     <script type="text/javascript">
         $(document).ready(function () {
             //jquery for toggle sub menus
@@ -87,4 +87,7 @@
         })
 
     </script>
+    @yield('scripts')
+
+    </body>
 </html>
